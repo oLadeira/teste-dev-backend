2f89d1fcb1dbd7e5420f49e24b4f4312a271b39a
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-04-04 15:15:49 -0300

    Initial commit

---
+++ dev/oli-saude/src/test/java/br/com/lucasladeira/olisaude/OliSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasladeira.olisaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
